11e7569d53d535f9e2b8f953dcf2b12997aac82c
Edgar_Garcia <83996283+edgarciavfx@users.noreply.github.com>
2021-08-18 11:57:43 -0500

    Add app breakdown

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      UserInput <br />
+        - NumberInput Bill/dollarIcon <br /> 
+        - TipInput <br />
+          - TipAmount 5 <br />
+          - TipAmount 10 <br />
+          - TipAmount 15 <br />
+          - TipAmount 25 <br />
+          - TipAmount 50 <br />
+          - TipCustom <br />
+        - NumberInput Number of People/peopleIcon <br />
+      UserOutput <br />
+        - PerPerson Tip Amount / math || value || function <br />
+        - PerPerson Tota l/ math || value || function <br />
+        - Reset onClick <br />
+      ----------------------------------------
       <div class="attribution">
         Challenge by{' '}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
