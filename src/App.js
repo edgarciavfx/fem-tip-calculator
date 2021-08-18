e2c4175bce892dd0578f8956e6d45061935c103c
Edgar_Garcia <83996283+edgarciavfx@users.noreply.github.com>
2021-08-18 11:41:08 -0500

    Add fem starter files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div class="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="https://github.com/edgarciavfx">edgarciavfx</a>.
+      </div>
     </div>
   );
 }
