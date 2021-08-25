524ceb3c7e3b96eb36a6ad1acd32e478e4aa34ae
Edgar_Garcia <83996283+edgarciavfx@users.noreply.github.com>
2021-08-25 11:12:22 -0500

    Add TipCalculator component

--- src/App.js
+++ src/App.js
@@ -1,79 +1,12 @@
-import { useState } from 'react';
-import NumberInput from './components/NumberInput';
-import TipInput from './components/TipInput';
-import logo from './images/logo.svg';
-import dollarIcon from './images/icon-dollar.svg';
-import personIcon from './images/icon-person.svg';
-import './styles/App.css';
-import PerPerson from './components/PerPerson';
-import ResetButton from './components/ResetButton';
+import React from 'react';
+import TipCalculator from './components/TipCalculator/TipCalculator';
 
-function App() {
-  const [bill, setBill] = useState(0);
-  const [tip, setTip] = useState(0);
-  const [people, setPeople] = useState(0);
-  const [isDisabled, setIsDisabled] = useState(true);
-
-  const tipCalc = (bill * tip) / 100;
-  const totalCalc = tipCalc + Number(bill);
-
-  const handleInput = (func) => (e) => {
-    func(e);
-    setIsDisabled(false);
-  };
-
-  const handleReset = () => {
-    refreshPage();
-  };
-
-  function refreshPage() {
-    window.location.reload();
-  }
-
+const App = () => {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <div className="App-card">
-        <div className="App-input">
-          <NumberInput
-            name="Bill"
-            icon={dollarIcon}
-            handleInput={handleInput(setBill)}
-          />
-          <TipInput handleInput={handleInput(setTip)} />
-          <NumberInput
-            name="Number of People"
-            icon={personIcon}
-            handleInput={handleInput(setPeople)}
-          />
-        </div>
-        <div className="App-output">
-          <PerPerson title="Tip Amount" tip={tipCalc} people={people} />
-          <PerPerson title="Total" tip={totalCalc} people={people} />
-          <ResetButton handleReset={handleReset} isDisabled={isDisabled} />
-        </div>
-      </div>
-      <div className="attribution">
-        Challenge by{' '}
-        <a
-          href="https://www.frontendmentor.io?ref=challenge"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by{' '}
-        <a
-          href="https://github.com/edgarciavfx"
-          target="_blank"
-          rel="noreferrer"
-        >
-          edgarciavfx
-        </a>
-        .
-      </div>
+    <div>
+      <TipCalculator />
     </div>
   );
-}
+};
 
 export default App;
